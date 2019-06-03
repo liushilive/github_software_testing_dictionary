84aa474da80ff18ade01c936bfe97ded33483b9f
刘士 <liushilive@outlook.com>
2019-06-03 18:19:36 +0800

    Create book.js

---
+++ book.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?921ac157b25b6c9fd562a97abf2806a7";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
