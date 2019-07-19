fcc5010a683f90e4b29ecaf95f7fc3d91e08fbc8
刘士 <liushilive@outlook.com>
2019-07-19 09:34:08 +0800

    1

--- book.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?921ac157b25b6c9fd562a97abf2806a7";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
